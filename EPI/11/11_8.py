772ddaacdd1fef04b9f430916448eeb16c218fb6
Brian Lee <briansmlee@gmail.com>
2018-01-06 18:20:47 +0900

    kth largest in O(n)

--- EPI/11/11_8.py
+++ EPI/11/11_8.py
@@ -25,16 +25,45 @@ if num < k - 1, find k - 1 - num more in lhs.
 if num > k - 1, in rhs, search for k again.
 
 5.Out
-def help(hi, lo, k):
-    x = rand
-    partition(A, x), and get idx of x.
-    if len(A) - idx == k, ret true
-    if >, lo = idx + 1 # more than k. rhs. 
-    if <, hi = idx - 1 # less than k. lhs, k -= len(A) - idx
-    
+rand int btw 
     
 6.Edge
 7.TS
+T(n) = O(n) + T(n/2)
+T(n) = O(n)
 """
+import random
+
+def kth_largest(A, k):
+    def partition(l, r, p):
+        p_val = A[p]
+        A[p], A[r] = A[r], A[p]
+        new_p = l
+        for i in range (l, r):
+            if p_val < A[i]:
+                A[new_p], A[i] = A[i], A[new_p]
+                new_p += 1
+        A[new_p], A[r] = A[r], A[new_p]
+        return new_p
+   
+    l, r = 0, len(A) - 1
+    while l <= r: 
+        rand_p = random.randint(l, r)
+        p = partition(l, r, rand_p)
+        if p > k - 1:
+            r = p - 1
+        elif p < k - 1:
+            l = p + 1
+        else:
+            return A[p]
+    return None 
+
+A = [3,2,1,5,4]
+print(kth_largest(A, 1))
+print(kth_largest(A, 2))
+print(kth_largest(A, 3))
+print(kth_largest(A, 4))
+print(kth_largest(A, 5))
+
 
 
